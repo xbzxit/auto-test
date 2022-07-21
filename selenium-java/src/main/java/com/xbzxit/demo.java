a0a213c045f0ec610a2beef036a0cab4651e772a
xbzxit <xbzxit@163.com>
2022-07-21 08:41:35 +0800

    selenium-java

---
+++ selenium-java/src/main/java/com/xbzxit/demo.java
@@ -0,0 +1,33 @@
+package com.xbzxit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * @author xbzxit
+ * @version 1.0
+ * @create 2022-07-20-21:46
+ * @company www.xbzxit.com
+ */
+
+public class demo {
+
+    public static void main(String[] args) {
+        WebDriver driver;
+
+//        System.setProperty("webdriver.firefox.bin","E:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        System.setProperty("webdriver.chrome.bin","\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\"");
+
+//        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+
+        driver.get("http://www.baidu.com");
+
+
+
+    }
+
+
+
+}
